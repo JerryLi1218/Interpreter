dad394282fef42d3137ec8365aedbafd4e94ab18
Jerry <2406700175@qq.com>
2023-09-11 12:53:08 +0800

    Update: set up Global Variables.

--- src/main/java/com/myinterpreter/lox/Parser.java
+++ src/main/java/com/myinterpreter/lox/Parser.java
@@ -20,7 +20,7 @@ public class Parser {
     List<Stmt> parse() {
         List<Stmt> statements = new ArrayList<>();
         while (!isAtEnd()) {
-            statements.add(statement());
+            statements.add(declaration());
         }
 
         return statements;
@@ -28,6 +28,17 @@ public class Parser {
 
     private Expr expression() {
         return equality();
+    }
+
+    private Stmt declaration() {
+        try {
+            if (match(VAR)) return varDeclaration();
+            
+            return statement();
+        } catch (ParseError error) {
+            synchronize();
+            return null;
+        }
     }
 
     private Stmt statement() {
@@ -40,6 +51,18 @@ public class Parser {
         Expr value = expression();
         consume(SEMICOLON, "Expect ';' afer value.");
         return new Stmt.Print(value);
+    }
+
+    private Stmt varDeclaration() {
+        Token name = consume(IDENTIFIER, "Expect variable name.");
+
+        Expr initializer = null;
+        if (match(EQUAL)) {
+            initializer = expression();
+        }
+
+        consume(SEMICOLON, "Expect ':' after variable declaration.");
+        return new Stmt.Var(name, initializer);
     }
 
     private Stmt expressionStatment() {
@@ -121,6 +144,10 @@ public class Parser {
         
         if (match(NUMBER, STRING)) {
             return new Expr.Literal(previous().literal);    
+        }
+
+        if(match(IDENTIFIER)) {
+            return new Expr.Variable(previous());
         }
         
         if (match(LEFT_PAREN)) {
