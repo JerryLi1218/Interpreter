5be5ef4c660bc8e67d4a3d31513c7b191d23c246
Jerry <2406700175@qq.com>
2023-09-12 19:48:04 +0800

    Update: set up Scope model.

--- src/main/java/com/myinterpreter/lox/Parser.java
+++ src/main/java/com/myinterpreter/lox/Parser.java
@@ -42,7 +42,8 @@ public class Parser {
     }
 
     private Stmt statement() {
-        if(match(PRINT)) return printStatement();
+        if (match(PRINT)) return printStatement();
+        if (match(LEFT_BRACE)) return new Stmt.Block(block());
 
         return expressionStatment();
     }
@@ -69,6 +70,17 @@ public class Parser {
         Expr expr = expression();
         consume(SEMICOLON, "Expect ';' afer value.");
         return new Stmt.Expression(expr);
+    }
+
+    private List<Stmt> block() {
+        List<Stmt> statements = new ArrayList<>();
+
+        while (!check(LEFT_BRACE) && !isAtEnd()) {
+            statements.add(declaration());
+        }
+
+        consume(RIGHT_BRACE, "Expect '}' after block.");
+        return statements;
     }
 
     private Expr assignment() {
